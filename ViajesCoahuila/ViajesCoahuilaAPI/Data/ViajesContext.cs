804dc922675ae2f295b1460074f3d032099ead66
iridero <jcgarza_mx@hotmail.com>
2023-05-02 17:52:42 -0600

    Ok

--- ViajesCoahuila/ViajesCoahuilaAPI/Data/ViajesContext.cs
+++ ViajesCoahuila/ViajesCoahuilaAPI/Data/ViajesContext.cs
@@ -1,6 +1,16 @@
-﻿namespace ViajesCoahuilaAPI.Data
+﻿using Microsoft.EntityFrameworkCore;
+using ViajesCoahuilaAPI.Models;
+
+namespace ViajesCoahuilaAPI.Data
 {
-    public class ViajesContext
+    public class ViajesContext:DbContext
     {
+        public DbSet<Usuario>   Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "database=viajes; password=root; user=root; server=localhost";
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+        }
     }
 }
