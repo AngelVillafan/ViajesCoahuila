21ddd7db56c0df55c2dd2eb3b9955e6e6cc6838d
iridero <jcgarza_mx@hotmail.com>
2023-05-02 17:41:10 -0600

    Creacion modelo usuario

---
+++ ViajesCoahuila/ViajesCoahuilaAPI/Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace ViajesCoahuilaAPI.Models
+{
+    /// <summary>
+    /// Representa un usuario de nuestra aplicación, 
+    /// puede tratarse de una persona que realiza
+    /// un viaje o alguien que busca un viaje (sin verde)
+    /// </summary>
+    public class Usuario
+    {
+    }
+}
