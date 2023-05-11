1c9cdd20237ecc24c5984564e8cd83d6b97f63e0
iridero <jcgarza_mx@hotmail.com>
2023-05-11 17:29:04 -0600

    Agregue cors

--- ViajesCoahuila/ViajesCoahuilaAPI/Program.cs
+++ ViajesCoahuila/ViajesCoahuilaAPI/Program.cs
@@ -2,13 +2,24 @@ using Microsoft.EntityFrameworkCore;
 using ViajesCoahuilaAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+            policy.AllowAnyOrigin();
+        });
+});
 
 string conn = builder.Configuration.GetConnectionString("ViajesConnection");
 
@@ -29,5 +40,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseCors(MyAllowSpecificOrigins);
 app.Run();
